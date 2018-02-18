b1bdb3eaf0d92e20d8097099ab64089bb09839fe
leedMx <esparza.mexico@gmail.com>
2018-02-18 15:07:56 -0600

    Included types.h to have a standard type size with compiler independence

--- kernel.cpp
+++ kernel.cpp
@@ -1,8 +1,9 @@
+#include "types.h"
 //main kernel function
 void printf(char* str)
 {
     //hard coded Video Memory address
-    unsigned short* VideoMemory = (unsigned short*)0xb8000;
+    uint16_t* VideoMemory = ( uint16_t* )0xb8000;
     //srcpy from the pointer
     for(int i=0;str[i] !=0; ++i)
       VideoMemory[i]= (VideoMemory[i] & 0xFF00) | str[i];//0xFF00 is the less significant part of the video mem bytes, meaning the color for background and foreground
@@ -20,7 +21,7 @@ extern "C" void callConstructors()
 
 //params accepting values on top of stack
 //extern "C" makes function available for linker
-extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
+extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
 {
   printf("We are now in kernel main");
   while(1);
